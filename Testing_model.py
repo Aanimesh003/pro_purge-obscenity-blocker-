31f456a6c9aebcbef80f55628fad52f02ee63a35
Animesh <iamanimesh02@gmail.com>
2023-08-02 23:55:55 +0530

    Added inital code

---
+++ Testing_model.py
@@ -0,0 +1,26 @@
+import keras
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+
+# Load the model
+model = keras.models.load_model("C:\\Users\\Animesh\\Downloads\\model_checkpoint_epoch.keras")
+
+# Load the image
+img = load_img("C:\\Users\\Animesh\\OneDrive - Amity University\\obscene_dataset\\Unsafe\\image_3 (2).jpg", target_size=(300, 300))
+
+# Convert the image to a numpy array
+input_data = img_to_array(img)
+
+# Normalize the pixel values
+input_data /= 255.0
+
+# Add a dimension to match the model's input shape
+input_data = np.expand_dims(input_data, axis=0)
+
+# Make a prediction
+prediction = model.predict(input_data)
+
+# Find the class with the highest probability
+predicted_class = np.argmax(prediction)
+
+print(predicted_class)
