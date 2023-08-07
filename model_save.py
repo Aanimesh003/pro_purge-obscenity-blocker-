3be7a2c1f9c190b509dec445c2091d1137d63a77
Rayan <ryanagent007@gmail.com>
2023-08-07 14:42:47 +0530

    model_save updated

--- model_save.py
+++ model_save.py
@@ -6,8 +6,8 @@ from keras.models import load_model
 from PIL import ImageFile
 
 def save_model_and_weights(model):
-      # Save the entire model (architecture + weights).
-   model_path = ('/media/ryana/Trainingstore/Best_Xception.keras')
+# Save the entire model (architecture + weights).
+   model_path = ('/media/ryana/Trainingstore/BestXception.keras')
    model.save(model_path)
    print("Model saved at epoch . Path:",model_path)
 
