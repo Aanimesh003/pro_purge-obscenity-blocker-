d80256f28043b5145b1f3789f5093f4380299cea
Animesh <iamanimesh02@gmail.com>
2023-08-07 18:48:41 +0530

    used the version 2.12.1 of tensorflow

--- from tensorflow.python.py
+++ from tensorflow.python.py
@@ -5,7 +5,8 @@ from keras.models import load_model
 
 model = load_model("C:\\Users\\Animesh\\Downloads\\categorical_classification_Xception.keras")
 
-input_signature = [tf.TensorSpec([3, 3], tf.float32, name='x')]
+input_shape = (300, 300, 3)
+input_signature = [tf.TensorSpec(shape=(None,) + input_shape, dtype=tf.float32)]
 # Use from_function for tf functions
 onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=13)
 onnx.save(onnx_model, "D:\\pythontt\\onnx_model\\model.onnx")
