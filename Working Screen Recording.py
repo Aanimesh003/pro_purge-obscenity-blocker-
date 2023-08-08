2cc97aa4729ca9331d67c9987496bfbeb0b8fa34
Rayan <ryanagent007@gmail.com>
2023-08-09 02:58:02 +0530

    Moved files

--- Working Screen Recording.py
+++ Working Screen Recording.py
@@ -1,11 +1,12 @@
 import time
 import numpy as np
 from classifier import Classifier
+from nudenet import NudeDetector
 import pyautogui
 import tkinter as tk
 import imgcompare
-from PIL import ImageGrab
 import image_utils
+from PIL import ImageGrab
 
 def close_overlay(overlay, freeze_screen):
     overlay.grab_release()
@@ -32,44 +33,45 @@ def show_overlay():
 
 i=0
 while True:
-    try:
-        #Optimization Required Need Faster Screenshot Implementation
-        screenshot = pyautogui.screenshot('pic.png')
-        time.sleep(0.2)
-        screenshot = pyautogui.screenshot('pic1.png')
-        is_same = imgcompare.is_equal("pic.png", "pic1.png", tolerance=1.5)
-        i=i+1
-    #    print("image taken")
-        if (is_same==False or i==10):
-            i=0
-            image_utils.splitimage("pic1.png")
+#    try:
+    #Optimization Required Need Faster Screenshot Implementation
+    screenshot = pyautogui.screenshot('pic.png')
+    time.sleep(0.2)
+    screenshot = pyautogui.screenshot('pic1.png')
+    is_same = imgcompare.is_equal("pic.png", "pic1.png", tolerance=1.5)
+    i=i+1
+#    print("image taken")
+    if (is_same==False or i==10):
+        i=0
+        image_utils.splitimage("pic1.png")
 
-            a = Classifier.classify("image_1.png")
-            safe_per=round(100*a["image_1.png"]['safe'],2)
-            print("safe_per_1",safe_per)
+        a = Classifier.classify("image_1.png")
+        safe_per=round(100*a["image_1.png"]['safe'],2)
+        print("safe_per_1",safe_per)
 
-            a = Classifier.classify("image_2.png")
-            safe_per1=round(100*a["image_2.png"]['safe'],2)
-            print("safe_per_2",safe_per)
+        a = Classifier.classify("image_2.png")
+        safe_per1=round(100*a["image_2.png"]['safe'],2)
+        print("safe_per_2",safe_per)
 
-            if safe_per < 40 or safe_per1 < 40 :
-                pyautogui.hotkey('alt', 'f4') #Keyboard inputs to Close Obscene window
-                # TO ADD: SEND OBSECNITY ALERT NOTIFICATION
-                # Proper Integration into a Fuction
-                # Launching a Script for Overlay that is a seperate file
-                root = tk.Tk()
-                root.attributes('-fullscreen', True)
-                root.withdraw()  # Hide the root window
+        if safe_per < 90 or safe_per1 < 90 :
+            l=NudeDetector.detect(self,"pic1.png")
+            pyautogui.hotkey('alt', 'f4') #Keyboard inputs to Close Obscene window
+            # TO ADD: SEND OBSECNITY ALERT NOTIFICATION
+            # Proper Integration into a Fuction
+            # Launching a Script for Overlay that is a seperate file
+            root = tk.Tk()
+            root.attributes('-fullscreen', True)
+            root.withdraw()  # Hide the root window
 
-                # Create a transparent window to freeze the screen
-                freeze_screen = tk.Toplevel(root)
-                freeze_screen.attributes('-fullscreen', True)
-                freeze_screen.attributes('-alpha', 0)  # Make the window transparent
-                freeze_screen.attributes('-topmost', True)  # Keep the transparent window on top of other windows
-                freeze_screen.grab_set_global()  # Grab all events to the transparent window
+            # Create a transparent window to freeze the screen
+            freeze_screen = tk.Toplevel(root)
+            freeze_screen.attributes('-fullscreen', True)
+            freeze_screen.attributes('-alpha', 0)  # Make the window transparent
+            freeze_screen.attributes('-topmost', True)  # Keep the transparent window on top of other windows
+            freeze_screen.grab_set_global()  # Grab all events to the transparent window
 
-                # Show the overlay after a short delay
-                root.after(100, show_overlay)
-                root.mainloop()
-    except:
-        print("Unable to take Sreenshot Device may be Asleep")
+            # Show the overlay after a short delay
+            root.after(100, show_overlay)
+            root.mainloop()
+#    except:
+#        print("Unable to take Sreenshot Device may be Asleep")
--- detector.py
+++ detector.py
@@ -1,62 +1,19 @@
 import os
-import cv2
 import pydload
-import logging
 import numpy as np
 import onnxruntime
-from progressbar import progressbar
-
-from .detector_utils import preprocess_image
-from .video_utils import get_interest_frames_from_video
-
+import cv2
+from detector_utils import preprocess_image
 
 def dummy(x):
     return x
 
-
-FILE_URLS = {
-    "default": {
-        "checkpoint": "https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_default_checkpoint.onnx",
-        "classes": "https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_default_classes",
-    },
-    "base": {
-        "checkpoint": "https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_base_checkpoint.onnx",
-        "classes": "https://github.com/notAI-tech/NudeNet/releases/download/v0/detector_v2_base_classes",
-},}
-
 class Detector:
-    detection_model = None
-    classes = None
+    global detection_model,classes
+    detection_model = "C:\Users\ryana\Downloads\detector_v2_base_checkpoint.onnx"
+    classes = "C:\Users\ryana\Downloads\detector_v2_default_classes"
 
-    def __init__(self, model_name="default"):
-        """
-        model = Detector()
-        """
-        checkpoint_url = FILE_URLS[model_name]["checkpoint"]
-        classes_url = FILE_URLS[model_name]["classes"]
-
-        home = os.path.expanduser("~")
-        model_folder = os.path.join(home, f".NudeNet/")
-        if not os.path.exists(model_folder):
-            os.makedirs(model_folder)
-
-        checkpoint_name = os.path.basename(checkpoint_url)
-        checkpoint_path = os.path.join(model_folder, checkpoint_name)
-        classes_path = os.path.join(model_folder, "classes")
-
-        if not os.path.exists(checkpoint_path):
-            print("Downloading the checkpoint to", checkpoint_path)
-            pydload.dload(checkpoint_url, save_to_path=checkpoint_path, max_time=None)
-
-        if not os.path.exists(classes_path):
-            print("Downloading the classes list to", classes_path)
-            pydload.dload(classes_url, save_to_path=classes_path, max_time=None)
-
-        self.detection_model = onnxruntime.InferenceSession(checkpoint_path)
-
-        self.classes = [c.strip() for c in open(classes_path).readlines() if c.strip()]
-
-    def detect(self, img_path, mode="default", min_prob=None):
+    def detect(img_path, mode="default", min_prob=None):
         if mode == "fast":
             image, scale = preprocess_image(img_path, min_side=480, max_side=800)
             if not min_prob:
@@ -66,9 +23,9 @@ class Detector:
             if not min_prob:
                 min_prob = 0.6
 
-        outputs = self.detection_model.run(
-            [s_i.name for s_i in self.detection_model.get_outputs()],
-            {self.detection_model.get_inputs()[0].name: np.expand_dims(image, axis=0)},
+        outputs = detection_model.run(
+            [s_i.name for s_i in detection_model.get_outputs()],
+            {detection_model.get_inputs()[0].name: np.expand_dims(image, axis=0)},
         )
 
         labels = [op for op in outputs if op.dtype == "int32"][0]
@@ -81,86 +38,13 @@ class Detector:
             if score < min_prob:
                 continue
             box = box.astype(int).tolist()
-            label = self.classes[label]
+            label = classes[label]
             processed_boxes.append(
                 {"box": [int(c) for c in box], "score": float(score), "label": label}
             )
-
         return processed_boxes
 
-    def detect_video(
-        self, video_path, mode="default", min_prob=0.6, batch_size=2, show_progress=True
-    ):
-        frame_indices, frames, fps, video_length = get_interest_frames_from_video(
-            video_path
-        )
-        logging.debug(
-            f"VIDEO_PATH: {video_path}, FPS: {fps}, Important frame indices: {frame_indices}, Video length: {video_length}"
-        )
-        if mode == "fast":
-            frames = [
-                preprocess_image(frame, min_side=480, max_side=800) for frame in frames
-            ]
-        else:
-            frames = [preprocess_image(frame) for frame in frames]
-
-        scale = frames[0][1]
-        frames = [frame[0] for frame in frames]
-        all_results = {
-            "metadata": {
-                "fps": fps,
-                "video_length": video_length,
-                "video_path": video_path,
-            },
-            "preds": {},
-        }
-
-        progress_func = progressbar
-
-        if not show_progress:
-            progress_func = dummy
-
-        for _ in progress_func(range(int(len(frames) / batch_size) + 1)):
-            batch = frames[:batch_size]
-            batch_indices = frame_indices[:batch_size]
-            frames = frames[batch_size:]
-            frame_indices = frame_indices[batch_size:]
-            if batch_indices:
-                outputs = self.detection_model.run(
-                    [s_i.name for s_i in self.detection_model.get_outputs()],
-                    {self.detection_model.get_inputs()[0].name: np.asarray(batch)},
-                )
-
-                labels = [op for op in outputs if op.dtype == "int32"][0]
-                scores = [op for op in outputs if isinstance(op[0][0], np.float32)][0]
-                boxes = [op for op in outputs if isinstance(op[0][0], np.ndarray)][0]
-
-                boxes /= scale
-                for frame_index, frame_boxes, frame_scores, frame_labels in zip(
-                    frame_indices, boxes, scores, labels
-                ):
-                    if frame_index not in all_results["preds"]:
-                        all_results["preds"][frame_index] = []
-
-                    for box, score, label in zip(
-                        frame_boxes, frame_scores, frame_labels
-                    ):
-                        if score < min_prob:
-                            continue
-                        box = box.astype(int).tolist()
-                        label = self.classes[label]
-
-                        all_results["preds"][frame_index].append(
-                            {
-                                "box": [int(c) for c in box],
-                                "score": float(score),
-                                "label": label,
-                            }
-                        )
-
-        return all_results
-
-    def censor(self, img_path, out_path=None, visualize=False, parts_to_blur=[]):
+    def censor(img_path, out_path=None, visualize=False, parts_to_blur=[]):
         if not out_path and not visualize:
             print(
                 "No out_path passed and visualize is set to false. There is no point in running this function then."
@@ -168,7 +52,7 @@ class Detector:
             return
 
         image = cv2.imread(img_path)
-        boxes = self.detect(img_path)
+        boxes = detect(img_path)
 
         if parts_to_blur:
             boxes = [i["box"] for i in boxes if i["label"] in parts_to_blur]
@@ -192,3 +76,30 @@ class Detector:
 if __name__ == "__main__":
     m = Detector()
     print(m.detect("/Users/bedapudi/Desktop/n2.jpg"))
+
+"""
+    def __init__(model_name="default"):
+        checkpoint_url = FILE_URLS[model_name]["checkpoint"]
+        classes_url = FILE_URLS[model_name]["classes"]
+
+        home = os.path.expanduser("~")
+        model_folder = os.path.join(home, f".NudeNet/")
+        if not os.path.exists(model_folder):
+            os.makedirs(model_folder)
+
+        checkpoint_name = os.path.basename(checkpoint_url)
+        checkpoint_path = os.path.join(model_folder, checkpoint_name)
+        classes_path = os.path.join(model_folder, "classes")
+
+        if not os.path.exists(checkpoint_path):
+            print("Downloading the checkpoint to", checkpoint_path)
+            pydload.dload(checkpoint_url, save_to_path=checkpoint_path, max_time=None)
+
+        if not os.path.exists(classes_path):
+            print("Downloading the classes list to", classes_path)
+            pydload.dload(classes_url, save_to_path=classes_path, max_time=None)
+
+        self.detection_model = onnxruntime.InferenceSession(checkpoint_path)
+
+        self.classes = [c.strip() for c in open(classes_path).readlines() if c.strip()]
+"""
