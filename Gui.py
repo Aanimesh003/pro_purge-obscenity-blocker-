1e9cb2dec06cae97594f740c7a1408703b1df54e
Divyansh <singhaldivyansh23@gmail.com>
2023-08-09 05:21:55 +0530

    modified Latest GUI

--- Gui.py
+++ Gui.py
@@ -57,28 +57,28 @@ class DeskApp:
 
         self.detect_video_button = tk.Button(self.root, text="Detect Video", command=self.detect_video, bg="green", font=("Helvetica", 12))
         self.detect_audio_button = tk.Button(self.root, text="Detect Audio", command=self.detect_audio, bg="green", font=("Helvetica", 12))
-        self.stop_button = tk.Button(self.root, text="Stop", command=self.stop_processes, bg="gray", font=("Helvetica", 12), state=tk.DISABLED)
+        self.stop_button = tk.Button(self.root, text="Stop", command=self.stop_processes, bg="gray", font=("Helvetica", 12))
 
         self.detect_video_button.pack(pady=10)
         self.detect_audio_button.pack(pady=10)
         self.stop_button.pack(pady=10)
 
-        self.recording = False
-        self.audio_detecting = False
-        self.video_thread = None
-        self.audio_thread = None
+        self.recording = True
+        self.audio_detecting = True
+        self.video_thread.start()
+        self.audio_thread.start()
 
     def detect_video(self):
-        self.detect_video_button.config(state=tk.DISABLED)
-        self.detect_audio_button.config(state=tk.DISABLED)
+        self.detect_video_button.config()
+        self.detect_audio_button.config()
         self.stop_button.config(state=tk.NORMAL)
 
         self.video_thread = threading.Thread(target=self.run_video_detection)
         self.video_thread.start()
 
     def detect_audio(self):
-        self.detect_video_button.config(state=tk.DISABLED)
-        self.detect_audio_button.config(state=tk.DISABLED)
+        self.detect_video_button.config()
+        self.detect_audio_button.config()
         self.stop_button.config(state=tk.NORMAL)
 
         self.audio_thread = threading.Thread(target=self.run_audio_detection)
@@ -86,27 +86,27 @@ class DeskApp:
 
     def run_video_detection(self):
         self.recording = True
-        self.detect_video_button.config(bg="red")  # Change button color to red
+        self.detect_video_button.config(bg="green")  
         while self.recording:
             subprocess.run(['python', 'Working Screen Recording.py'])
-            print("Detecting video...")
+            
 
     def run_audio_detection(self):
         self.audio_detecting = True
-        self.detect_audio_button.config(bg="red")  # Change button color to red
+        self.detect_audio_button.config(bg="green")  
         while self.audio_detecting:
             subprocess.run(['python', 'beeping_obscenity.py'])
-            print("Detecting audio...")
+            
 
     def stop_processes(self):
         self.recording = False
         self.audio_detecting = False
         if self.video_thread:
-            self.video_thread.join()  # Wait for the video thread to finish
-            self.detect_video_button.config(state=tk.NORMAL, bg="green")
+              
+            self.detect_video_button.config(state=tk.NORMAL, bg="red")
         if self.audio_thread:
-            self.audio_thread.join()  # Wait for the audio thread to finish
-            self.detect_audio_button.config(state=tk.NORMAL, bg="green")
+              
+            self.detect_audio_button.config(state=tk.NORMAL, bg="red")
         self.detect_video_button.config(state=tk.NORMAL)
         self.detect_audio_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.DISABLED)
