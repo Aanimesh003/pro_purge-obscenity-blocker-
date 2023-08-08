5b72e30e98bc62d210090d8fd2f54141c3cc2c7f
Rayan <ryanagent007@gmail.com>
2023-08-09 04:42:24 +0530

    Added NudeDetector Implemented NudeDetector in Screen Recording

--- Working Screen Recording.py
+++ Working Screen Recording.py
@@ -7,7 +7,7 @@ import tkinter as tk
 import imgcompare
 import image_utils
 from PIL import ImageGrab
-
+detector = NudeDetector()
 def close_overlay(overlay, freeze_screen):
     overlay.grab_release()
     overlay.destroy()
@@ -33,45 +33,49 @@ def show_overlay():
 
 i=0
 while True:
-#    try:
-    #Optimization Required Need Faster Screenshot Implementation
-    screenshot = pyautogui.screenshot('pic.png')
-    time.sleep(0.2)
-    screenshot = pyautogui.screenshot('pic1.png')
-    is_same = imgcompare.is_equal("pic.png", "pic1.png", tolerance=1.5)
-    i=i+1
-#    print("image taken")
-    if (is_same==False or i==10):
-        i=0
-        image_utils.splitimage("pic1.png")
+    try:
+        #Optimization Required Need Faster Screenshot Implementation
+        #screenshot = pyautogui.screenshot('pic.png')
+        time.sleep(0.2)
+        screenshot = pyautogui.screenshot('pic1.png')
+        is_same = imgcompare.is_equal("pic.png", "pic1.png", tolerance=.15)
+        i=i+1
+    #    print("image taken")
+        if True:#(is_same==False or i==10):
+            i=0
+            image_utils.splitimage("pic1.png")
 
-        a = Classifier.classify("image_1.png")
-        safe_per=round(100*a["image_1.png"]['safe'],2)
-        print("safe_per_1",safe_per)
+            a = Classifier.classify("image_1.png")
+            safe_per=round(100*a["image_1.png"]['safe'],2)
+            print("safe_per_1",safe_per)
 
-        a = Classifier.classify("image_2.png")
-        safe_per1=round(100*a["image_2.png"]['safe'],2)
-        print("safe_per_2",safe_per)
+            a1 = Classifier.classify("image_2.png")
+            safe_per1=round(100*a1["image_2.png"]['safe'],2)
+            print("safe_per_2",safe_per1)
 
-        if safe_per < 90 or safe_per1 < 90 :
-            l=NudeDetector.detect(self,"pic1.png")
-            pyautogui.hotkey('alt', 'f4') #Keyboard inputs to Close Obscene window
-            # TO ADD: SEND OBSECNITY ALERT NOTIFICATION
-            # Proper Integration into a Fuction
-            # Launching a Script for Overlay that is a seperate file
-            root = tk.Tk()
-            root.attributes('-fullscreen', True)
-            root.withdraw()  # Hide the root window
+            if safe_per < 60 or safe_per1 < 60 :
+                l=detector.detect("pic1.png") #Keyboard inputs to Close Obscene window
+                if l[0]['score']:
+                    point=l[0]['box'][0],l[0]['box'][1]
+                    pyautogui.moveTo(point)
+                    pyautogui.leftClick()
+                    pyautogui.hotkey('alt', 'f4')
+                # TO ADD: SEND OBSECNITY ALERT NOTIFICATION
+                # Proper Integration into a Fuction
+                # Launching a Script for Overlay that is a seperate file
+                root = tk.Tk()
+                root.attributes('-fullscreen', True)
+                root.withdraw()  # Hide the root window
 
-            # Create a transparent window to freeze the screen
-            freeze_screen = tk.Toplevel(root)
-            freeze_screen.attributes('-fullscreen', True)
-            freeze_screen.attributes('-alpha', 0)  # Make the window transparent
-            freeze_screen.attributes('-topmost', True)  # Keep the transparent window on top of other windows
-            freeze_screen.grab_set_global()  # Grab all events to the transparent window
+                # Create a transparent window to freeze the screen
+                freeze_screen = tk.Toplevel(root)
+                freeze_screen.attributes('-fullscreen', True)
+                freeze_screen.attributes('-alpha', 0)  # Make the window transparent
+                freeze_screen.attributes('-topmost', True)  # Keep the transparent window on top of other windows
+                freeze_screen.grab_set_global()  # Grab all events to the transparent window
 
-            # Show the overlay after a short delay
-            root.after(100, show_overlay)
-            root.mainloop()
-#    except:
-#        print("Unable to take Sreenshot Device may be Asleep")
+                # Show the overlay after a short delay
+                root.after(100, show_overlay)
+                root.mainloop()
+    except:
+        print("Unable to take Sreenshot Device may be Asleep")
