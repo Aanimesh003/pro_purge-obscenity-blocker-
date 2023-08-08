0d5de3de6712f36a1ca25cc7f5248cf75ac78337
Rayan <ryanagent007@gmail.com>
2023-08-08 14:52:54 +0530

    Reduced Checking rate

--- Working Screen Recording.py
+++ Working Screen Recording.py
@@ -3,6 +3,7 @@ import numpy as np
 from nudenet import NudeClassifier
 import pyautogui
 import tkinter as tk
+import imgcompare
 
 
 def close_overlay(overlay, freeze_screen):
@@ -29,34 +30,41 @@ def show_overlay():
     overlay.after(2000, close_overlay, overlay, freeze_screen)
 
 classifier = NudeClassifier()
-
+i=0
 while True:
     #Optimization Required Need Faster Screenshot Implementation
     screenshot = pyautogui.screenshot('pic.png')
+    screenshot = pyautogui.screenshot('pic1.png')
+    is_same = imgcompare.is_equal("pic.png", "pic1.png", tolerance=2.5)
+    i=i+1
+    print(i)
+#    print("image taken")
+    if (is_same==False or i==10):
+        i=0
+        a = classifier.classify("pic.png")
+
+        safe_per=round(100*a["pic.png"]['safe'],2)      
     
-    a = classifier.classify("pic.png")
+        if safe_per < 40 :
+            pyautogui.hotkey('alt', 'f4') #Keyboard inputs to Close Obscene window
+            # TO ADD: SEND OBSECNITY ALERT NOTIFICATION
+            # Proper Integration into a Fuction
+            # Launching a Script for Overlay that is a seperate file
+            root = tk.Tk()
+            root.attributes('-fullscreen', True)
+            root.withdraw()  # Hide the root window
 
-    safe_per=round(100*a["pic.png"]['safe'],2)      
-  
-    if safe_per < 40 :
-        pyautogui.hotkey('alt', 'f4') #Keyboard inputs to Close Obscene window
-        # TO ADD: SEND OBSECNITY ALERT NOTIFICATION
-        # Proper Integration into a Fuction
-        # Launching a Script for Overlay that is a seperate file
-        root = tk.Tk()
-        root.attributes('-fullscreen', True)
-        root.withdraw()  # Hide the root window
+            # Create a transparent window to freeze the screen
+            freeze_screen = tk.Toplevel(root)
+            freeze_screen.attributes('-fullscreen', True)
+            freeze_screen.attributes('-alpha', 0)  # Make the window transparent
+            freeze_screen.attributes('-topmost', True)  # Keep the transparent window on top of other windows
+            freeze_screen.grab_set_global()  # Grab all events to the transparent window
 
-        # Create a transparent window to freeze the screen
-        freeze_screen = tk.Toplevel(root)
-        freeze_screen.attributes('-fullscreen', True)
-        freeze_screen.attributes('-alpha', 0)  # Make the window transparent
-        freeze_screen.attributes('-topmost', True)  # Keep the transparent window on top of other windows
-        freeze_screen.grab_set_global()  # Grab all events to the transparent window
+            # Show the overlay after a short delay
+            root.after(100, show_overlay)
+            root.mainloop()
 
-        # Show the overlay after a short delay
-        root.after(100, show_overlay)
-        root.mainloop()
-
-            
-    print("safe",safe_per,"%")
+                
+        print("safe",safe_per,"%")
+        time.sleep(0.3)
