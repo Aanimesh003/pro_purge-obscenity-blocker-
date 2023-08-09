c7ea4e9c0c4f9f602b0a2c3351f576c7fc5ace79
Divyansh <singhaldivyansh23@gmail.com>
2023-08-09 06:23:38 +0530

    added the enter functionality in Gui

--- Gui.py
+++ Gui.py
@@ -22,11 +22,13 @@ class DeskApp:
         self.username_entry = tk.Entry(self.login_frame, font=("Helvetica", 12))
         self.username_entry.insert(0, "Username:")  # Placeholder
         self.username_entry.bind("<FocusIn>", self.clear_placeholder)
+        self.username_entry.bind("<Return>", lambda event: self.authenticate())
         self.username_entry.pack(pady=10)
 
         self.password_entry = tk.Entry(self.login_frame, show="*", font=("Helvetica", 12))
         self.password_entry.insert(0, "Password:")  # Placeholder
         self.password_entry.bind("<FocusIn>", self.clear_placeholder)
+        self.password_entry.bind("<Return>", lambda event: self.authenticate())
         self.password_entry.pack(pady=10)
 
         login_button = tk.Button(self.login_frame, text="Login", command=self.authenticate, font=("Helvetica", 12))
@@ -57,7 +59,7 @@ class DeskApp:
 
         self.detect_video_button = tk.Button(self.root, text="Detect Video", command=self.detect_video, bg="green", font=("Helvetica", 12))
         self.detect_audio_button = tk.Button(self.root, text="Detect Audio", command=self.detect_audio, bg="green", font=("Helvetica", 12))
-        self.stop_button = tk.Button(self.root, text="Stop", command=self.stop_processes, bg="gray", font=("Helvetica", 12))
+        self.stop_button = tk.Button(self.root, text="Stop", command=self.stop_processes, bg="gray", font=("Helvetica", 12), state=tk.DISABLED)
 
         self.detect_video_button.pack(pady=10)
         self.detect_audio_button.pack(pady=10)
@@ -65,47 +67,41 @@ class DeskApp:
 
         self.recording = True
         self.audio_detecting = True
+        self.video_thread = threading.Thread(target=self.run_video_detection)
+        self.audio_thread = threading.Thread(target=self.run_audio_detection)
         self.video_thread.start()
         self.audio_thread.start()
 
     def detect_video(self):
-        self.detect_video_button.config()
-        self.detect_audio_button.config()
+        self.detect_video_button.config(state=tk.DISABLED)
+        self.detect_audio_button.config(state=tk.DISABLED)
         self.stop_button.config(state=tk.NORMAL)
-
-        self.video_thread = threading.Thread(target=self.run_video_detection)
-        self.video_thread.start()
 
     def detect_audio(self):
-        self.detect_video_button.config()
-        self.detect_audio_button.config()
+        self.detect_video_button.config(state=tk.DISABLED)
+        self.detect_audio_button.config(state=tk.DISABLED)
         self.stop_button.config(state=tk.NORMAL)
 
-        self.audio_thread = threading.Thread(target=self.run_audio_detection)
-        self.audio_thread.start()
-
     def run_video_detection(self):
-        self.recording = True
         self.detect_video_button.config(bg="green")  
         while self.recording:
             subprocess.run(['python', 'Working Screen Recording.py'])
-            
+            print("Detecting video...")
 
     def run_audio_detection(self):
-        self.audio_detecting = True
         self.detect_audio_button.config(bg="green")  
         while self.audio_detecting:
             subprocess.run(['python', 'beeping_obscenity.py'])
-            
+            print("Detecting audio...")
 
     def stop_processes(self):
         self.recording = False
         self.audio_detecting = False
         if self.video_thread:
-              
+            self.video_thread.join()  
             self.detect_video_button.config(state=tk.NORMAL, bg="red")
         if self.audio_thread:
-              
+            self.audio_thread.join()  
             self.detect_audio_button.config(state=tk.NORMAL, bg="red")
         self.detect_video_button.config(state=tk.NORMAL)
         self.detect_audio_button.config(state=tk.NORMAL)
