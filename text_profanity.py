4451718c0f09010fd5f9fbea5c27441602c8540b
Rayan <ryanagent007@gmail.com>
2023-08-09 14:29:24 +0530

    Added Text Profainty Detection

--- text_profanity.py
+++ text_profanity.py
@@ -7,18 +7,13 @@ from better_profanity import profanity
 
 # Defining paths to tesseract.exe
 # and the image we would be using
-path_to_tesseract = r"Z:\\Tesseract-OCR\\tesseract.exe"
+path_to_tesseract = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 image_path = r"pic.png"
 
 # Opening the image & storing it in an image object
 img = Image.open(image_path)
 
-# Providing the tesseract executable
-# location to pytesseract library
 pytesseract.tesseract_cmd = path_to_tesseract
-
-# Passing the image object to image_to_string() function
-# This function will extract the text from the image
 text = pytesseract.image_to_string(img)
 
 
